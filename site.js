f18874809f6414b169b9cc00aac64065f5f98d43
PiousPigeon <145592758+PiousPigeon@users.noreply.github.com>
2025-08-19 23:42:39 -0700

    Move Linktree button to top of Projects menu
    - Linktree button now appears first in Projects dropdown
    - Dynamic projects list moved below the separator
    - Better organization with external link at top, project categories below

--- site.js
+++ site.js
@@ -144,9 +144,9 @@ class SimpleBlog {
           <div class="menu-item" data-menu="projects">
             <div class="label">Projects</div>
             <div class="menu-dropdown" id="projects-dropdown">
-              <div class="menu-entry" id="projects-menu">Loading...</div>
-              <div class="menu-separator"></div>
               <a class="menu-entry" href="https://linktr.ee/PiousPigeon" target="_blank">Linktree</a>
+              <div class="menu-separator"></div>
+              <div class="menu-entry" id="projects-menu">Loading...</div>
             </div>
           </div>
           
